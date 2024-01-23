c6ec8aefd61e71fa87b4fdcb563d63f0aeb58673
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-01-23 13:07:09 +0100

    add social meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,31 @@
+const TITLE = 'Борис Надеждин 2024: счетчики сбора подписей и статистика';
+const DESCRIPTION = 'Отслеживание сбора подписей за кандидата в презеденты Бориса Надеждина, cстатистика по регионам';
+const KEYWORDS = 'Надеждин,2024,выборы,подписи,статистика,счетчик';
+const IMAGE_URL = 'https://raw.githubusercontent.com/bnad24/bnad24.github.io/main/public/social.png';
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="ru">
       <head>
-        <title>{'Борис Надеждин: статистика сбора подписей'}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>{TITLE}</title>
+        <meta name="title" content={TITLE} />
+        <meta name="description" content={DESCRIPTION} />
+        <meta name="keywords" content={KEYWORDS} />
+        <meta name="robots" content="index, nofollow" />
+        <meta name="revisit-after" content="1 days" />
+        <meta property="og:title" content={TITLE} />
+        <meta property="og:description" content={DESCRIPTION} />
+        <meta property="og:image" content={IMAGE_URL} />
+        <meta name="twitter:title" content={TITLE} />
+        <meta name="twitter:description" content={DESCRIPTION} />
+        <meta name="twitter:image" content={IMAGE_URL} />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐘</text></svg>"
+        />
       </head>
       <body>{children}</body>
     </html>
