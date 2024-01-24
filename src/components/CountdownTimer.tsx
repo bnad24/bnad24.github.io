cb55ce4add3f8f9cac495a6ec8f065bcd5f31ce6
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-01-24 21:24:21 +0100

    fix countdown date

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -15,13 +15,13 @@ export function CountdownTimer() {
       <p>
         <b>{'Окончание сбора подписей: чт, 25 янв. 2024 г., 23:59:59 мск.'}</b>
         <br />
-        <Countdown date={DateTime.fromISO('2024-01-25T22:59:59.999Z').toJSDate()} renderer={renderer} />
+        <Countdown date={DateTime.fromISO('2024-01-25T20:59:59.999Z').toJSDate()} renderer={renderer} />
       </p>
 
       <p>
         <b>{'Срок подачи подписей в ЦИК: ср, 31 янв. 2024 г., 23:59:59 мск.'}</b>
         <br />
-        <Countdown date={DateTime.fromISO('2024-01-31T22:59:59.999Z').toJSDate()} renderer={renderer} />
+        <Countdown date={DateTime.fromISO('2024-01-31T20:59:59.999Z').toJSDate()} renderer={renderer} />
       </p>
     </div>
   );
