6f335a9126b81f1a6c309e5aadf86c3d44076674
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-01-25 03:24:08 +0100

    improve countdown final text

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -3,25 +3,29 @@ import Countdown from 'react-countdown';
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
-    return null;
+    return <span>{'Что, всё? Может и нет. Уточняйте в официальных источниках!'}</span>;
   } else {
     return <span>{`Осталось: ${days} д. ${hours} ч. ${minutes} мин. ${seconds} с.`}</span>;
   }
 };
 
 export function CountdownTimer() {
+  const d1 = DateTime.fromISO('2024-01-25T20:59:59.999Z');
+  const d2 = DateTime.fromISO('2024-01-31T20:59:59.999Z');
+  const now = DateTime.utc();
+
   return (
     <div>
       <p>
-        <b>{'Окончание сбора подписей: чт, 25 янв. 2024 г., 23:59:59 мск.'}</b>
+        <b className={now > d1 && 'strike'}>{'Окончание сбора подписей: чт, 25 янв. 2024 г., 23:59:59 мск.'}</b>
         <br />
-        <Countdown date={DateTime.fromISO('2024-01-25T20:59:59.999Z').toJSDate()} renderer={renderer} />
+        <Countdown date={d1.toJSDate()} renderer={renderer} />
       </p>
 
       <p>
-        <b>{'Срок подачи подписей в ЦИК: ср, 31 янв. 2024 г., 23:59:59 мск.'}</b>
+        <b className={now > d2 && 'strike'}>{'Срок подачи подписей в ЦИК: ср, 31 янв. 2024 г., 23:59:59 мск.'}</b>
         <br />
-        <Countdown date={DateTime.fromISO('2024-01-31T20:59:59.999Z').toJSDate()} renderer={renderer} />
+        <Countdown date={d2.toJSDate()} renderer={renderer} />
       </p>
     </div>
   );
