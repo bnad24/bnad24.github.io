5fa9031074af84141237d6c4e932970bf94a0b6f
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-02-03 23:35:49 +0100

    add logo

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,6 +8,9 @@ import { usePathname } from 'next/navigation';
 export function Navigation() {
   return (
     <ul className="navigation-ul">
+      <Link href="/" style={{ marginRight: '1rem' }}>
+        <img height="16px" src="/logo.png" alt="Логотип сайта" />
+      </Link>
       <NavLink href="/">{'Главная'}</NavLink>
       <NavLink href="/sigs">{'Подписи'}</NavLink>
       <NavLink href="/addresses">{'Штабы'}</NavLink>
