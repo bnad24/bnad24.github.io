be13f09ca6335a2faacb77a476640c03be09c1b6
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-02-03 22:45:35 +0100

    add favicon, manifest, sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <head>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%2210 0 100 100%22><text y=%22.90em%22 font-size=%2290%22>🐘</text></svg>"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#4b8ad8" />
+        <link rel="shortcut icon" href="favicon.ico" />
+        <meta name="msapplication-TileColor" content="#c6d3e3" />
+        <meta name="msapplication-TileImage" content="favicons/mstile-144x144.png" />
+        <meta name="msapplication-config" content="favicons/browserconfig.xml" />
+        <meta name="theme-color" content="#c6d3e3" />
       </head>
       <body>
         <Providers>
---
+++ src/app/sitemap.ts
@@ -0,0 +1,42 @@
+import fs from 'fs-extra';
+import { MetadataRoute } from 'next';
+import { DOMAIN } from '../constants';
+import { MemesJson } from '../types';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { memes } = (await fs.readJSON('public/data/memes.json')) as MemesJson;
+  const memeEntries = memes.map((meme) =>
+    createSitemapEntry({
+      url: `/memes/${meme.id}`,
+      priority: 0.95,
+    }),
+  );
+
+  return [
+    createSitemapEntry({ url: '/', priority: 1 }),
+    createSitemapEntry({ url: '/sigs', priority: 0.5 }),
+    createSitemapEntry({ url: '/addresses', priority: 0.5 }),
+    createSitemapEntry({ url: '/memes', priority: 0.9 }),
+    ...memeEntries,
+  ];
+}
+
+type SitemapEntry = MetadataRoute.Sitemap[0];
+type ChangeFrequency = SitemapEntry['changeFrequency'];
+
+function createSitemapEntry({
+  url,
+  changeFrequency,
+  priority,
+}: {
+  url: string;
+  changeFrequency?: ChangeFrequency;
+  priority?: number;
+}): SitemapEntry {
+  return {
+    url: `${DOMAIN}${url}`,
+    lastModified: new Date(),
+    changeFrequency: changeFrequency ?? 'daily',
+    priority: priority ?? 1,
+  };
+}
