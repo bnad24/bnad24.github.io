11f4688aab1723d30202c615d1378615893159ef
z-x-y-a <157440852+z-x-y-a@users.noreply.github.com>
2024-02-03 03:41:34 +0100

    add more memes

--- scripts/memes-update.mjs
+++ scripts/memes-update.mjs
@@ -1,21 +1,27 @@
 import jquery from 'jquery';
 import { JSDOM } from 'jsdom';
-import { sum, first, uniq, get, sortBy } from 'lodash-es';
+import { sum, first, last, uniq, get, sortBy } from 'lodash-es';
 import fs from 'fs-extra';
 import { DateTime } from 'luxon';
 import { dirname } from 'path';
 
 async function main() {
+  const input = await fs.readJSON('public/data/memes.json');
+  const lastId = last(sortBy(input.memes, (x) => x.id))?.id ?? '';
+
   let memes = (await fs.readdir('public/content/memes/', { withFileTypes: true, recursive: true }))
     .filter((item) => !item.isDirectory())
     .map((item) => {
       const id = item.name.replace(/\.(jpg|png|gif)$/, '');
       const url = item.path.replace(/^public/, '') + item.name;
       return { url, id, name: '', description: '' };
-    });
+    })
+    .filter((item) => item.id > lastId);
+
+  memes = [...input.memes, ...memes];
   memes = sortBy(memes, (x) => -x.id);
 
-  console.log({ memes });
+  console.log(memes);
 
   const updatedAt = DateTime.now().toUTC().toISO();
   await fs.writeJson('public/data/memes.json', { memes, updatedAt }, { spaces: 2 });
